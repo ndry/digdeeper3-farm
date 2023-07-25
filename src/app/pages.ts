29c22abe025f55cb0d927a5c25d04eeef8a48402
Koraus <dmytro.kashchynets@gmail.com>
2023-07-25 10:47:35 +0200

    added ca page

--- src/app/pages.ts
+++ src/app/pages.ts
@@ -2,3 +2,4 @@ export { default as nb_nb_2023_07_06 } from "../nb/nb-2023-07-06";
 export { default as nb_tf_webgl_benchmark } from "../nb/tf-predict-benchmark";
 export { default as nb_tf_predict_benchmark } from "../nb/tf-predict-benchmark";
 export { default as nb_batch_run } from "../nb/batch-run";
+export { default as ca_page } from "../nb/ca-page";
---
+++ src/nb/ca-page/index.tsx
@@ -0,0 +1,15 @@
+import { retroThemeCss } from "../nb-2023-07-06/retro-theme-css";
+
+export default function App() {
+
+    return <div css={
+        [{
+            fontSize: "0.7em",
+            display: "flex",
+            flexDirection: "column",
+            padding: "1em",
+        }, retroThemeCss]
+    }>
+        CA page
+    </div>;
+}
