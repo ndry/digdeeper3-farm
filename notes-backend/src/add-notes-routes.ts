6181b381a356db8a96ddb77190a2d4917ec990a0
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-07-27 12:08:50 +0300

    parse values

--- notes-backend/src/add-notes-routes.ts
+++ notes-backend/src/add-notes-routes.ts
@@ -1,14 +1,15 @@
 import { RouterType, json } from "itty-router";
 import { Env } from "./env";
 import { noteFromText } from "core/note23727v1";
+import { _never } from "core/utils/_never";
 
 
 export const addNotesRoutes = (router: RouterType) => router
     .get("/notes/", async (req, env: Env) => {
         const keys = await env.NOTES_KV.list();
         const values = await Promise.all(keys.keys.map(async key => {
-            const value = await env.NOTES_KV.get(key.name);
-            return { key: key.name, value };
+            const value = await env.NOTES_KV.get(key.name) ?? _never();
+            return { key: key.name, value: JSON.parse(value) };
         }));
         return json(values);
     })
