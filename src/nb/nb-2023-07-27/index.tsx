a7bedcb909adefcbb376ae80e5e8cc00c6bc49c3
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-07-27 20:26:37 +0300

    typings tweakes

--- src/nb/nb-2023-07-27/index.tsx
+++ src/nb/nb-2023-07-27/index.tsx
@@ -1,7 +1,7 @@
 import { retroThemeCss } from "../nb-2023-07-06/retro-theme-css";
 import usePromise from "react-use-promise";
 import jsonBeautify from "json-beautify";
-import { implementation } from "../../ca237v1/rule-io";
+import { isRule } from "../../ca237v1/rule-io";
 import { RulePreview } from "../../app/rule-preview";
 
 
@@ -11,7 +11,8 @@ const notesUrl = "https://hq.x-pl.art/notes/";
 export default function App() {
     const [notes, notesError, notesStatus] = usePromise(async () => {
         const response = await fetch(notesUrl);
-        const notes = await response.json();
+        const notes = await response.json() as Array<
+            { key: string, value: unknown }>;
         notes.reverse();
         return notes;
     }, []);
@@ -28,12 +29,13 @@ export default function App() {
         Hello World
         <div>{notesStatus}</div>
         <div>{notesError?.stack}</div>
-        {notes?.map((note: unknown, i: number) => {
+        {notes?.map((note, i) => {
             const rule =
-                note.value
+                !!note.value
                 && (typeof note.value === "object")
+                && ("tags" in note.value)
                 && Array.isArray(note.value.tags)
-                && note.value.tags.find(tag => tag.startsWith(implementation));
+                && note.value.tags.find(isRule);
 
             return <div key={i}>
                 {rule && <div><RulePreview code={rule} /></div>}
