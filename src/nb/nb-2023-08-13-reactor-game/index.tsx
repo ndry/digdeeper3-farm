5728c04a0cee1fccc732f8cbdeb6d304dd5f34b4
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-08-15 16:34:01 +0300

    very basic life

--- src/nb/nb-2023-08-13-reactor-game/index.tsx
+++ src/nb/nb-2023-08-13-reactor-game/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { retroThemeCss } from "../nb-2023-07-06/retro-theme-css";
 import jsonBeautify from "json-beautify";
 import update from "immutability-helper";
@@ -17,12 +17,31 @@ import { rrr } from "./run-reactor";
 
 
 
+
 const asciiStateMap = ["·", "ı", "x"] as const;
 
+
+
+
+export const scoreSubstance = (substance: Rule, target: Rule) => {
+    const t1 = parseTable(substance);
+    const t2 = parseTable(target);
+    let score = 0;
+    for (let i = 0; i < t1.length; i++) {
+        score += t1[i] === t2[i] ? 1 : 0;
+    }
+    return score;
+};
+
+
+
 export function SubstanceView({
-    substance, ...props
+    substance,
+    scoreTarget,
+    ...props
 }: jsx.JSX.IntrinsicElements["span"] & {
     substance: Rule,
+    scoreTarget?: Rule,
 }) {
     const padLen = 47 - substance.length;
     const pad = Array.from({ length: padLen }, () => ".").join("");
@@ -37,13 +56,16 @@ export function SubstanceView({
         </a>
         &nbsp;/&nbsp;
         {parseTable(substance).map(d => asciiStateMap[d])}
+        {scoreTarget && <>&nbsp;/&nbsp;{scoreSubstance(substance, scoreTarget).toString()}</>}
     </span>;
 }
 
 export function ReactionView({
     rule, reagent1, reagent2, t,
+    scoreTarget,
 }: {
     rule: Rule, reagent1: Rule, reagent2: Rule, t: number,
+    scoreTarget?: Rule,
 }) {
     const table = parseTable(rule);
     const spacetime = [
@@ -77,26 +99,71 @@ export function ReactionView({
             &#x269B;
             <SubstanceView
                 substance={keyifyTable(space)}
+                scoreTarget={scoreTarget}
             />
         </div>)}
     </div>;
 }
 
+export const createWorld = () => ({
+    t: 0,
+    creatures: [] as Array<{
+        rule: Rule,
+        energy: number,
+    }>,
+    soup: [] as Rule[],
+});
+export const tickInPlace = (worldState: {
+    t: number;
+    creatures: Array<{
+        rule: Rule,
+        energy: number,
+    }>,
+    soup: Rule[],
+}) => {
+    for (let i = worldState.creatures.length - 1; i >= 0; i--) {
+        const c = worldState.creatures[i];
+        const s = worldState.soup.shift();
+        const score = s ? scoreSubstance(s, c.rule) : 0;
+        c.energy += score - 20;
+        if (c.energy > 81) {
+            worldState.creatures.push({
+                rule: c.rule,
+                energy: 27,
+            });
+            worldState.creatures.push({
+                rule: c.rule,
+                energy: 27,
+            });
+            c.energy -= 27 * 2;
+        } else if (c.energy <= 0) {
+            worldState.creatures.splice(i, 1);
+        }
+    }
+    worldState.creatures.push({
+        rule: ca237v1FromSeed(SHA256("creature." + worldState.t)),
+        energy: 27,
+    });
+    worldState.t++;
+};
+
+
+
 export default function Component() {
-    const target = useMemo(() => ca237v1FromSeed(SHA256("target")), []);
-
-    // const x = useMemo(() => createAutoRecipe(s0, target), [target]);
-    const x = useMemo(() => {
-        const log = [] as Parameters<typeof rrr>[0];
-
-        const t0 = rrr(log, s0, s0, s0, 6);
-        const t1 = rrr(log, t0, s0, s0, 22);
-        const t2 = rrr(log, t1, s0, s0, 22);
-        const t3 = rrr(log, t2, s0, t1, 5);
-        const t4 = rrr(log, s0, t1, t3, 1);
-
-        return log;
-    }, [target]);
+    const [renderTrigger, setRenderTrigger] = useState(0);
+    const s1 = useMemo(() => ca237v1FromSeed(SHA256("s1")), []);
+    const world = useMemo(createWorld, []);
+    console.log(renderTrigger);
+    useEffect(() => {
+        const h = setInterval(() => {
+            for (let i = 0; i < 100; i++) {
+                world.soup.push(s1);
+            }
+            tickInPlace(world);
+            setRenderTrigger(renderTrigger => renderTrigger + 1);
+        }, 50);
+        return () => clearInterval(h);
+    }, [world]);
 
     return (
         <div css={[{
@@ -107,13 +174,49 @@ export default function Component() {
         }, retroThemeCss]}>
             Hello World from {import.meta.url}
             <br />
-            s0: <SubstanceView substance={s0} />
-
-            <br />
-            .t: <SubstanceView substance={target} />
-            <br />
-            <JsonButton name="x" obj={x} />
-            {x.map((reaction, i) => <ReactionView key={i} {...reaction} />)}
+            render: {renderTrigger}
+            <pre>{jsonBeautify(world, null as any, 2, 80)}</pre>
         </div >
     );
 }
+
+
+// export default function Component() {
+//     const target = useMemo(() => ca237v1FromSeed(SHA256("target")), []);
+
+//     // const x = useMemo(() => createAutoRecipe(s0, target), [target]);
+//     const x = useMemo(() => {
+//         const log = [] as Parameters<typeof rrr>[0];
+
+//         const t0 = rrr(log, s0, s0, s0, 6);
+//         const t1 = rrr(log, t0, s0, s0, 22);
+//         const t2 = rrr(log, t1, s0, s0, 22);
+//         const t3 = rrr(log, t2, s0, t1, 5);
+//         const t4 = rrr(log, s0, t1, t3, 1);
+
+//         return log;
+//     }, [target]);
+
+//     return (
+//         <div css={[{
+//             fontSize: "0.71em",
+//             // display: "flex",
+//             // flexDirection: "column",
+//             padding: "1em",
+//         }, retroThemeCss]}>
+//             Hello World from {import.meta.url}
+//             <br />
+//             s0: <SubstanceView substance={s0} />
+
+//             <br />
+//             .t: <SubstanceView substance={target} />
+//             <br />
+//             <JsonButton name="x" obj={x} />
+//             {x.map((reaction, i) => <ReactionView
+//                 key={i}
+//                 {...reaction}
+//                 scoreTarget={target}
+//             />)}
+//         </div >
+//     );
+// }
