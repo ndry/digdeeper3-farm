b9ba8ed6b3c580b75dfa1d05952df8c93d30e071
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-08-25 16:51:53 +0300

    sorted by firstRepeatAt

--- src/nb/nb-2023-08-25-reactor-game/index.tsx
+++ src/nb/nb-2023-08-25-reactor-game/index.tsx
@@ -2,7 +2,7 @@ import { useLayoutEffect, useMemo, useRef } from "react";
 import { retroThemeCss } from "../nb-2023-07-06/retro-theme-css";
 import { SHA256 } from "crypto-js";
 import { jsx } from "@emotion/react";
-import { Rule, parseTable } from "../../ca237v1/rule-io";
+import { Rule, keyifyTable, parseTable } from "../../ca237v1/rule-io";
 import { ca237v1FromSeed } from "../nb-2023-08-13-reactor-game/ca237v1-from-seed";
 import { LinkCaPreview } from "../nb-2023-08-13-reactor-game/link-ca-preview";
 import { createImageData32, cssColorToAbgr } from "../../utils/create-image-data32";
@@ -17,49 +17,68 @@ const colorMap = [
     cssColorToAbgr("#0000ff"),
 ];
 
+export function calcutateRule(rule: Rule) {
+    const spaceSet = new Set<Rule>([start0, start1]);
+    let firstRepeatAt = Infinity;
+
+    const tCap = 2000;
+
+    const imageData = new ImageData(tCap, 81);
+    const imageData32 = createImageData32(imageData);
+
+    const table = parseTable(rule);
+    let prevPrev = parseTable(start0);
+    let prev = parseTable(start1);
+
+    for (let x = 0; x < prevPrev.length; x++) {
+        imageData32.setPixelAbgr(0, x, colorMap[prevPrev[x]]);
+        imageData32.setPixelAbgr(1, x, colorMap[prev[x]]);
+    }
+
+    let space = prev.map(() => 0);
+    for (let t = 2; t < tCap; t++) {
+        for (let x = 0; x < prev.length; x++) {
+            space[x] = table[getFullCombinedState(
+                3,
+                prev[(x - 1 + prev.length) % prev.length],
+                prev[x],
+                prev[(x + 1) % prev.length],
+                prevPrev[x],
+            )];
+            imageData32.setPixelAbgr(t, x,
+                t === firstRepeatAt + 1 ? 0 : colorMap[space[x]]);
+        }
+
+        const r = keyifyTable(space);
+        if (spaceSet.has(r)) {
+            if (t < firstRepeatAt) {
+                firstRepeatAt = t;
+            }
+        } else {
+            spaceSet.add(r);
+        }
+
+        [prevPrev, prev, space] = [prev, space, prevPrev];
+    }
+
+
+    return {
+        firstRepeatAt,
+        imageData,
+    };
+}
+
 export function RuleView({
     name,
     rule,
+    imageData,
+    firstRepeatAt,
     ...props
-}: jsx.JSX.IntrinsicElements["div"] & {
+}: jsx.JSX.IntrinsicElements["div"] & ReturnType<typeof calcutateRule> & {
     name: string,
     rule: Rule,
 }) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
-
-    const { imageData } = useMemo(() => {
-        const tCap = 1000;
-
-        const imageData = new ImageData(tCap, 81);
-        const imageData32 = createImageData32(imageData);
-
-        const table = parseTable(rule);
-        let prevPrev = parseTable(start0);
-        let prev = parseTable(start1);
-
-        for (let x = 0; x < prevPrev.length; x++) {
-            imageData32.setPixelAbgr(0, x, colorMap[prevPrev[x]]);
-            imageData32.setPixelAbgr(1, x, colorMap[prev[x]]);
-        }
-
-        let space = prev.map(() => 0);
-        for (let t = 2; t < tCap; t++) {
-            for (let x = 0; x < prev.length; x++) {
-                space[x] = table[getFullCombinedState(
-                    3,
-                    prev[(x - 1 + prev.length) % prev.length],
-                    prev[x],
-                    prev[(x + 1) % prev.length],
-                    prevPrev[x],
-                )];
-                imageData32.setPixelAbgr(t, x, colorMap[space[x]]);
-            }
-            [prevPrev, prev, space] = [prev, space, prevPrev];
-        }
-
-
-        return { imageData };
-    }, []);
 
     useLayoutEffect(() => {
         const canvasEl = canvasRef.current;
@@ -74,6 +93,8 @@ export function RuleView({
     return <div {...props}>
         {name}:
         <LinkCaPreview substance={rule} />
+        &nbsp;/&nbsp;
+        firstRepeatAt: {firstRepeatAt}
         <br />
         <canvas
             ref={canvasRef}
@@ -86,11 +107,14 @@ export default function Component() {
     const rules = useMemo(
         () => Array.from({ length: 100 }, (_, i) => {
             const seed = rootSeed + "." + i;
+            const rule = ca237v1FromSeed(SHA256(seed));
+            const calc = calcutateRule(rule);
             return {
-                rule: ca237v1FromSeed(SHA256(seed)),
+                rule,
                 seed,
-            }
-        }),
+                ...calc,
+            };
+        }).sort((a, b) => a.firstRepeatAt - b.firstRepeatAt),
         [rootSeed],
     );
 
