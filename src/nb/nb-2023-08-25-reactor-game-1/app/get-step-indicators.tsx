94e41557fa34a871d49e70fd04b25497210a1be8
Koraus <dmytro.kashchynets@gmail.com>
2023-08-31 15:20:16 +0200

    removed memoization from the step count

--- src/nb/nb-2023-08-25-reactor-game-1/app/get-step-indicators.tsx
+++ src/nb/nb-2023-08-25-reactor-game-1/app/get-step-indicators.tsx
@@ -1,11 +1,9 @@
 import { ReactionCard } from "../model/reaction-card";
-import memoize from "memoizee";
 
-export const getStepIndicators = memoize((
-    reactions: ReactionCard[],
-): {
+
+export function getStepIndicators(reactions: ReactionCard[]): {
     steps: number, repeatAt: number, stepRatio: number
-} => {
+} {
     let steps = 0;
     let repeatAt = 0;
 
@@ -18,4 +16,4 @@ export const getStepIndicators = memoize((
         repeatAt,
         stepRatio: repeatAt !== 0 ? steps / repeatAt : 0,
     };
-});
+}
